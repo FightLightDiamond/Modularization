8339fe536a5dd17ce249244857dbc51bfb66bb11
cuongpm <cuongpm@Macs-MacBook-Pro-2.local>
2019-09-05 10:43:16 +0700

    Update ModelTrait & API docs

--- src/MultiInheritance/ModelsTrait.php
+++ src/MultiInheritance/ModelsTrait.php
@@ -40,8 +40,19 @@ trait ModelsTrait
                 continue;
             }
 
-            if($field === 'sort') {
+            if ($field === 'sort') {
                 $query->sortBy($value);
+                continue;
+            }
+
+            if ($field === 'my') {
+                $query->my();
+                continue;
+            }
+
+            if ($field === 'relationship') {
+                $query->with($value);
+                continue;
             }
         }
 
@@ -57,50 +68,33 @@ trait ModelsTrait
 
     public function scopeOrders($query, $input = [])
     {
-        foreach ($input as $field => $value) {
-            $query->orderBy($this->table . '.' . $field, $value);
+        foreach ($input as $field => $sortBy) {
+            $query->orderBy($this->table . '.' . $field, $sortBy);
         }
 
-        return $query;
-    }
-
-    //========================ACTION============================>
-
-    public function checkbox($input)
-    {
-        if (isset($this->checkbox)) {
-            foreach ($this->checkbox as $value) {
-                (isset($input[$value]) && $input[$value] !== '0') ? $input[$value] = 1 : $input[$value] = 0;
-            }
-        }
-        return $input;
-    }
-
-    public function uploadImport($file)
-    {
-        $newName = FormatFa::reFileName($file);
-        $file->storeAs(
-            $this->table, $newName
-        );
-        return storage_path('app/' . $this->table . '/' . $newName);
-    }
-
-    public function scopeRelation($query, $input)
-    {
-        if (isset($input['relations'])) {
-            foreach ($input['relations'] as $relation) {
-                $query->with($relation);
-            }
-        }
         return $query;
     }
 
     public function scopeMy($query, $field = 'user_id')
     {
-        if(in_array($field, $this->fillable)) {
+        if (in_array($field, $this->fillable)) {
             return $query->where($this->table . '.' . $field, Auth::id());
         }
 
         return $query;
+    }
+
+
+    //========================ACTION============================>
+
+    public function uploadImport($file)
+    {
+        $newName = FormatFa::reFileName($file);
+
+        $file->storeAs(
+            $this->table, $newName
+        );
+
+        return storage_path("app/{$this->table}/{$newName}");
     }
 }
